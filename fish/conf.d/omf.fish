7894e2fae2831486ad68b6b9688687b207b95c17
sid <juniorlechado@gmail.com>
2020-08-01 19:22:44 -0300

    💻

---
+++ fish/conf.d/omf.fish
@@ -0,0 +1,16 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
+
+set --export ANDROID_HOME ~/Android/Sdk;
+set -gx PATH $ANDROID_HOME/tools $PATH;
+set -gx PATH $ANDROID_HOME/tools/bin $PATH;
+set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+set -gx PATH $ANDROID_HOME/emulator $PATH
+
+set --export JAVA_HOME /usr/lib/jvm/java-8-openjdk/;
+set -gx PATH $JAVA_HOME/bin $PATH;
